6435641bb39cd9d37d2abec9cf33f1fef580f5a8
Ernesto Cervantes <ernesto.acacej@gmail.com>
2019-11-28 22:43:05 -0600

    Correct workflow

--- main.py
+++ main.py
@@ -25,52 +25,28 @@ blockedQueue = []
 processStatus = {}
 clk = 0
 llega_length = 3
-PID_actual
+PID_actual = 0
 
 logLines = []
 quantum = 0
 case = 0 # 0 RR   1 prioNonPreemptive
 
-txtFile = open(sys.argv[1])
-for line in txtFile:
-    logLines.append(line)
-
-#case = str.strip(logLines[0])
-quantum = int(logLines[1][7:])
-
-for log in logLines:
-    log = str.strip(log)
-    words = log.split()
-    if words[1] == "Llega":
-        llega(words)
-    elif words[1] == "Acaba":
-        acaba(words)
-    elif words[1] == "startIO":
-        startIO(words)
-    elif words[1] == "endIO":
-        endIO(words)
-    elif words[1] == "endSimulacion":
-        endSimulacion(words)
-    else:
-        error("")
-    
-    print(words)
-
 
 def llega(words):
     global waitQueue
     numbers = validateLength(words, llega_length)
-    pID = numbers[2]
+    pID = numbers[1]
     if pID in processStatus:
         error("Ya estaba el proceso")
     else:
         processStatus[pID] = 'waitQueue'
         waitQueue.append(pID)
 
+
 def acaba(words):
     global processStatus, waitQueue, blockedQueue
     numbers = validateLength(words, 3)
-    pID = numbers[2]
+    pID = numbers[1]
     if not pID in processStatus:
         error("No estaba el proceso")
     else:
@@ -82,10 +58,11 @@ def acaba(words):
             changePIDactual(False)
         processStatus.pop(pID)
 
+
 def startIO(words):
     global processStatus, waitQueue, blockedQueue
     numbers = validateLength(words, 3)
-    pID = numbers[2]
+    pID = numbers[1]
     if not pID in processStatus:
         error("No estaba el proceso")
     else:
@@ -99,10 +76,11 @@ def startIO(words):
             blockedQueue.append(pID)
         processStatus[pID] = 'blockedQueue'
 
+
 def endIO(words):
     global processStatus, waitQueue, blockedQueue
     numbers = validateLength(words, 3)
-    pID = numbers[2]
+    pID = numbers[1]
     if not pID in processStatus:
         error("No estaba el proceso")
     else:
@@ -113,17 +91,58 @@ def endIO(words):
         else:
             error("no estaba en la lista")
 
+
 def endSimulacion(words):
     pass
 
+
 def error(message):
     sys.exit(message)
+
 
 def validateLength(words, length):
     if len(words) != length:
         error("Faltaron argumentos")
     return [int(s) for s in words if s.isdigit()]
 
+
 def changePIDactual(goToWait):
     global PID_actual
+    if goToWait:
+        waitQueue.append(PID_actual)
+        processStatus[PID_actual] = 'waitQueue'
+    if waitQueue:
+        PID_actual = waitQueue.pop(0)
+        processStatus[PID_actual] = 'running'
+
+
+
+txtFile = open(sys.argv[1])
+for line in txtFile:
+    logLines.append(line)
+
+#case = str.strip(logLines[0])
+quantum = int(logLines[1][7:])
+
+logLines.pop(0)
+logLines.pop(0)
+
+for log in logLines:
+    log = str.strip(log)
+    words = log.split()
+    if words[1] == "Llega":
+        llega(words)
+    elif words[1] == "Acaba":
+        acaba(words)
+    elif words[1] == "startI/O":
+        startIO(words)
+    elif words[1] == "endI/O":
+        endIO(words)
+    elif words[1] == "endSimulacion":
+        endSimulacion(words)
+    else:
+        error("error de cod")
     
+    print('processStatus', processStatus)
+
+
